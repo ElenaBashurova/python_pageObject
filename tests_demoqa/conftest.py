a14e6a07fd07360f219c4a1eb1706d3ffd22970d
Elena Bashurova <110354016+ElenaBashurova@users.noreply.github.com>
2024-01-04 20:30:55 +0300

    2 часть

--- tests_demoqa/conftest.py
+++ tests_demoqa/conftest.py
@@ -10,22 +10,22 @@ def setup_browser(request):
     browser.config.window_width = 1920
     browser.config.window_height = 1080
     browser.config.base_url = 'https://demoqa.com'
-    options = Options()
-    selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": "100.0",
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": True
-        }
-    }
-    options.capabilities.update(selenoid_capabilities)
-    driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options
-    )
-
-    browser.config.driver = driver
+    # options = Options()
+    # selenoid_capabilities = {
+    #     "browserName": "chrome",
+    #     "browserVersion": "100.0",
+    #     "selenoid:options": {
+    #         "enableVNC": True,
+    #         "enableVideo": True
+    #     }
+    # }
+    # options.capabilities.update(selenoid_capabilities)
+    # driver = webdriver.Remote(
+    #     command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+    #     options=options
+    # )
+    #
+    # browser.config.driver = driver
 
     yield
 
