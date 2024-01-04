ac473c7700b453a32f6998e8db9cae07ba89f76c
Elena Bashurova <110354016+ElenaBashurova@users.noreply.github.com>
2024-01-05 00:57:34 +0300

    2 часть

--- tests_demoqa/conftest.py
+++ tests_demoqa/conftest.py
@@ -13,8 +13,8 @@ def load_env():
     load_dotenv()
 @pytest.fixture(scope='function')
 def setup_browser(request):
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
+    browser.config.window_width = 1080
+    browser.config.window_height = 1920
     browser.config.base_url = 'https://demoqa.com'
     options = Options()
     selenoid_capabilities = {
